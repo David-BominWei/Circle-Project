fe5d3f4b68f6b8ee4cdb4d843e9f27c22f5839f0
nagisaikarii <weibomindd@163.com>
2023-11-18 17:25:00 -0800

    init dataset

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Circle-Project
+
+> The project is aim to build a model which is able to find the circle inside an image using CNN network
+
+## Files
+
++ `/circle/*`: the folder include all helper functions
++ `circleDataset.py`: the dataset class for initialize the Datasets for training the model. All training data are generated using the `generate_examples` function with default parameter. The following options are included:
+  + `FixCircleDataset(img_size, datasize)`: a list of `datasize` number of data is generated and fixed for training the model
---
+++ circleDataset.py
@@ -0,0 +1,41 @@
+from circle.circle_detection_data import generate_examples
+from torch.utils.data import Dataset
+import numpy as np
+from torch import tensor
+from tqdm import tqdm
+
+class FixCircleDataset(Dataset):
+    """
+    Generate a list of circles and set as the dataset, the dataset will be fixed and used for training the model
+    """
+    
+    def __init__(self, img_size=100, data_size=10000):
+        """
+        Args:
+            img_size:       The size of image
+            data_size:      The number of data in the dataset
+        """
+        
+        super().__init__()
+        circle_iter = generate_examples(img_size=img_size)
+        circle_list = []
+        
+        #generate the list of circle
+        for _ in tqdm(range(data_size)):
+            circle_list.append(next(circle_iter)[0])
+        
+        # set the data as the list circles
+        self.circle_data = tensor(np.array(circle_list))
+        self.data_size = data_size
+        
+    def __getitem__(self, index):
+        return self.circle_data[index]
+    
+    def __len__(self):
+        return self.data_size
+    
+if __name__ == "__main__":
+    from torch.utils.data import DataLoader
+    
+    test_loader = DataLoader(FixCircleDataset(), batch_size=64, shuffle=True)
+    print(next(iter(test_loader)).shape)
